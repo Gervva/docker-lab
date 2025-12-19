46c3bb436233f8507f2cb154ab0b26b01302d2bf
Gervva <germanvladimr@yandex.ru>
2025-12-19 16:27:50 +0300

    lab9

--- app2/app2.py
+++ app2/app2.py
@@ -3,10 +3,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/hello')
+@app.route("/") # hello лишнее, иначе не будет открываться localhost/
 def hello_world():
-    return render_template('index.html')
+    return render_template('index.html', source_app='app2')
 
 
 if __name__ == '__main__':
-   app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(host="0.0.0.0", port=8080) # был невалидный порт
